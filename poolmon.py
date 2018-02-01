23f8aa5f9ba543b71f11c7a70c993f02da161a16
nerdralph <ralphdoncaster@gmail.com>
2018-02-01 19:17:45 -0400

    time mining.subscribe response

--- poolmon.py
+++ poolmon.py
@@ -17,11 +17,13 @@ for pool in pools:
 	s.connect(pool)
 	socks.append(s)
 	s.sendall('{"id": 0, "worker": "m0",  "method": "eth_submitLogin", "params": ["0xeb9310b185455f863f526dab3d245809f6854b4d"]}\n'.encode())
-	#s.sendall('{"id": 1, "method": "mining.subscribe", "params": []}\n'.encode())
+	response = s.recv(2048)
+	print (response)
+	s.sendall('{"id": 1, "method": "mining.subscribe", "params": []}\n'.encode())
 	start = time.time()
 	response = s.recv(2048)
-	print(time.time() - start)
 	print (response)
+	print(time.time() - start, s.getpeername())
 
 print("Waiting for data.")
 start = time.time()
@@ -29,7 +31,6 @@ while socks:
 	readable, writable, errs = select.select(socks, [], socks)
 	for s in readable:
 		response = s.recv(2048)
-		print ("%4.6f"% (time.time() - start))
-		print (s.getpeername())
-		#print (response)
+		print ("%4.6f"% (time.time() - start), s.getpeername())
+		print (response)
 
